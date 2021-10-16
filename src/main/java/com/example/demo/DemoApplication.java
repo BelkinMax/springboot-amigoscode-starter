7649219f6ea64a7e28844b161a92dbd1b8a21ccd
Maxim Belkin <belkinurban@gmail.com>
2021-10-16 15:04:20 +0200

    Create Student class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +18,16 @@ public class DemoApplication {
     }
 
     @GetMapping
-    public List<String> hello() {
-        return List.of("Hello", "World", "!");
+    public List<Student> hello() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Max",
+                        "belkinurban@gmail.com",
+                        LocalDate.of(1995, 9, 5),
+                        26
+                )
+        );
     }
 
 }
